838bd148a81a2207c96ccb90e5150d77831c57fa
dayfundora <dayfundoraglez@gmail.com>
2021-03-05 00:14:14 -0500

    Extract Entity Names

--- information_extraction.py
+++ information_extraction.py
@@ -24,3 +24,15 @@ def chunked_sentences(text):
     tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
     chunk_sentences = nltk.ne_chunk_sents(tagged_sentences, binary=True)
     return chunk_sentences
+
+def extract_entity_names(t):
+    entity_names = []
+
+    if hasattr(t, 'label') and t.label:
+        if t.label() == 'NE':
+            entity_names.append(' '.join([child[0] for child in t]))
+        else:
+            for child in t:
+                entity_names.extend(extract_entity_names(child))
+
+    return entity_names
